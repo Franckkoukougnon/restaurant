1134ab6640b6999880999886ff6a8fab077263ba
franck <franckkoukougnon@yahoo.fr>
2022-12-05 14:44:47 +0100

    exercice algo java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        exo_methode.maMethod(5, 7);
+
+        exo_methode.unMessage("J’ai compris comment faire une méthode !");
+    }
+
+
+
+}
---
+++ src/exo_methode.java
@@ -0,0 +1,14 @@
+public class exo_methode {
+    static void maMethod(int nb1, int nb2){
+        System.out.println(nb1 + nb2);
+    }
+    static  void moText(String name){
+        System.out.println("Hello "+name);
+    }
+    static void unMessage(String message){
+        System.out.println(message);
+    }
+
+
+
+}
